27f86dad29841bb04eac12135bff5d4eccc3cb07
Adi Narayana Reddy <adinarayana4444@gmail.com>
2023-01-17 23:39:43 +0530

    Added Filter inputs

--- src/pages/Landing/LandingPage.jsx
+++ src/pages/Landing/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../common/Header/Navbar";
+import Filters from "./components/Filters/Filters";
 import "./LandingPage.css";
 
 const LandingPage = () => {
 	return (
 		<div>
 			<Navbar />
+			<Filters />
 		</div>
 	);
 };
--- src/pages/Landing/components/Filters/Filters.jsx
+++ src/pages/Landing/components/Filters/Filters.jsx
@@ -4,7 +4,58 @@ import "./Filters.css";
 const Filters = () => {
 	return (
 		<div>
-			<h1>Filters</h1>
+			<div className='colors-div'>
+				<label>
+					<input type='checkbox' />
+					Red
+				</label>
+				<label>
+					<input type='checkbox' />
+					Blue
+				</label>
+				<label>
+					<input type='checkbox' />
+					Green
+				</label>
+			</div>
+			<div className='gneder-div'>
+				<label>
+					<input type='checkbox' />
+					Men
+				</label>
+				<label>
+					<input type='checkbox' />
+					Women
+				</label>
+			</div>
+			<div className='price-div'>
+				<label>
+					<input type='checkbox' />
+					Rs. 0-250
+				</label>
+				<label>
+					<input type='checkbox' />
+					Rs. 251-450
+				</label>
+				<label>
+					<input type='checkbox' />
+					Rs. 250
+				</label>
+			</div>
+			<div className='type-div'>
+				<label>
+					<input type='checkbox' />
+					Pole
+				</label>
+				<label>
+					<input type='checkbox' />
+					Hoodie
+				</label>
+				<label>
+					<input type='checkbox' />
+					Basic
+				</label>
+			</div>
 		</div>
 	);
 };
